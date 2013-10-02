e06f73b11ed35c5338d86a43c3bc6da25111e8ca
fkelly <frank.kelly@nokia.com>
2013-10-01 21:04:17 -0400

    COnvert to Junit 3

--- src/test/java/com/kellyfj/codingkata/roman/RomanNumeralUtilsTest.java
+++ src/test/java/com/kellyfj/codingkata/roman/RomanNumeralUtilsTest.java
@@ -1,18 +1,14 @@
 package com.kellyfj.codingkata.roman;
 
-import static org.junit.Assert.assertEquals;
+import junit.framework.TestCase;
 
-import org.junit.Test;
+public class RomanNumeralUtilsTest extends TestCase {
 
-public class RomanNumeralUtilsTest {
-
-    @Test
     public void testGetRomanNumerals() {
         String r = RomanNumeralUtils.getRomanNumerals(1978);
         assertEquals("MCMLXXVIII",r);
     }
     
-    @Test
     public void testGetInt() {
         int i = RomanNumeralUtils.getInt("MCMLXXVIII");
         assertEquals(1978,i);
