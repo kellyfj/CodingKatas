60c903bd31bff19cd2eba12dd621d1a9c06e5fa9
fkelly <frank.kelly@nokia.com>
2013-10-14 20:38:50 -0400

    Format changes

--- src/main/java/com/kellyfj/codingkata/arrays/NumberArrays.java
+++ src/main/java/com/kellyfj/codingkata/arrays/NumberArrays.java
@@ -11,22 +11,21 @@ public class NumberArrays {
 
     /**
      * http://codercareer.blogspot.com/2011/10/no-09-numbers-with-given-sum.html
-     * Given an array, please determine whether it contains three numbers whose sum equals to 0.
-     */    
+     * Given an array, please determine whether it contains three numbers whose
+     * sum equals to 0.
+     */
     public static boolean doesSumOfTwoEqualN(int[] array, int target) {
-       
-        if(array.length < 2)
+        if (array.length < 2)
             throw new IllegalArgumentException("Array length should be 2 or greater");
-        
         Arrays.sort(array);
         
-        int startPointer=0;
-        int endPointer = array.length-1;
-        
-        while(startPointer < endPointer) {
+        int startPointer = 0;
+        int endPointer = array.length - 1;
+        while (startPointer < endPointer) {
             int sum = array[startPointer] + array[endPointer];
-            if(sum == target) {
-                System.out.println("Numbers ["+array[startPointer] +"] and ["+array[endPointer]+"] sum to ["+target+"]");
+            if (sum == target) {
+                System.out.println("Numbers [" + array[startPointer] + "] and [" + array[endPointer] + "] sum to ["
+                        + target + "]");
                 return true;
             } else {
                 if (sum > target) {
@@ -37,123 +36,118 @@ public class NumberArrays {
             }
         }
         return false;
-    }  
-        
-        public static boolean doesSumOfThreeEqualN(int[] array, int target) {
-            if(array.length < 3)
-                throw new IllegalArgumentException("At least 3 elements expected in the array");
-            Arrays.sort(array);
-            
-            int pointer = 0;
-            while(pointer < array.length-1) {
-                int sumOfTwoTarget = target - array[pointer];
-                boolean res = doesSumOfTwoEqualN(array, sumOfTwoTarget);
-                if(res){
-                    
-                    return true;
-                }
-                pointer++;
-            }
-            return false;
-        }
-        
-        public static void shuffle(Object[] a) {
-            Random rnd = new Random();
-            int mid = a.length / 2;
-            for (int i = mid; i < a.length; i++) {
-                int lo = rnd.nextInt(mid);
-                Object buffer = a[lo];
-                a[lo] = a[i];
-                a[i] = buffer;
-            }
-        }
-        
-        public static void printArray(Object[] a) {
-            for(int i=0; i< a.length; i++) {
-                System.out.print(a[i].toString()+ " ");
-            }
-            System.out.println();
-        }
-        
-        public static void printArray(int[] a) {
-            for(int i=0; i< a.length; i++) {
-                System.out.print(a[i]+ " ");
-            }
-            System.out.println();
-        }
-        
-        public static int lengthOfLongestMonotonicSeries(int[] input) {
+    }
 
-            int max=1;
-            int num=1;
-            for(int i=0; i< input.length-1; i++) {
- 
-                if(input[i] == input[i+1]-1) {
-                    num++;
-                    max=num>max ? num: max; 
-                } else {
-                    num = 0;
-                }
+    public static boolean doesSumOfThreeEqualN(int[] array, int target) {
+        if (array.length < 3)
+            throw new IllegalArgumentException("At least 3 elements expected in the array");
+        Arrays.sort(array);
+        int pointer = 0;
+        while (pointer < array.length - 1) {
+            int sumOfTwoTarget = target - array[pointer];
+            boolean res = doesSumOfTwoEqualN(array, sumOfTwoTarget);
+            if (res) {
+                return true;
             }
-            
-            return max;
+            pointer++;
         }
-        
-        
-        public static void dutchNationalFlagBasic(int[] input)
-        {
-            int numZeroes=0;
-            int numOnes=0;
-            int numTwos=0;
-            
-            for(int i=0; i< input.length; i++) {
-                if(input[i]==0)
-                    numZeroes++;
-                else if(input[i] ==1)
-                       numOnes++;
-                else if(input[i]==2)
-                    numTwos++;
-            }
-            
-            for(int i=0; i< numZeroes; i++)
-                input[i]=0;
-            for(int i=numZeroes; i< numZeroes+numOnes; i++)
-                input[i]=1;    
-            for(int i=numZeroes+numOnes; i< numZeroes+numOnes+numTwos; i++)
-                    input[i]=2;
-            
-        }
-        
-        public static void dutchNationalFlagOnePass(int[] a, int mid) {
-            int startIndex = 0;
-            int endIndex = a.length - 1;
-            int temp;
+        return false;
+    }
 
-            for (int i = 0; i <= endIndex ;) {
-                if (a[i] < mid) {
-                    temp = a[i];
-                    a[i] = a[startIndex];
-                    a[startIndex] = temp;
-                    i++;
-                    startIndex++;
-                } else if (a[i] > mid) {
-                    temp = a[i];
-                    a[i] = a[endIndex];
-                    a[endIndex] = temp;
-                    endIndex--;
-                    // do not increment i. We have to revisit this again
-                } else {
-                    i++;
-                }
+    public static void shuffle(Object[] a) {
+        Random rnd = new Random();
+        int mid = a.length / 2;
+        for (int i = mid; i < a.length; i++) {
+            int lo = rnd.nextInt(mid);
+            Object buffer = a[lo];
+            a[lo] = a[i];
+            a[i] = buffer;
+        }
+    }
+
+    public static void printArray(Object[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i].toString() + " ");
+        }
+        System.out.println();
+    }
+
+    public static void printArray(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static int lengthOfLongestMonotonicSeries(int[] input) {
+
+        int max = 1;
+        int num = 1;
+        for (int i = 0; i < input.length - 1; i++) {
+
+            if (input[i] == input[i + 1] - 1) {
+                num++;
+                max = num > max ? num : max;
+            } else {
+                num = 0;
             }
         }
-        
-        
-        public static int countBinaryOnes(int number) {
-            String binaryString = Integer.toBinaryString(number);
-            return binaryString.length() - binaryString.replace("1", "").length();
+
+        return max;
+    }
+
+    public static void dutchNationalFlagBasic(int[] input) {
+        int numZeroes = 0;
+        int numOnes = 0;
+        int numTwos = 0;
+
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] == 0)
+                numZeroes++;
+            else if (input[i] == 1)
+                numOnes++;
+            else if (input[i] == 2)
+                numTwos++;
         }
-        
+
+        for (int i = 0; i < numZeroes; i++)
+            input[i] = 0;
+        for (int i = numZeroes; i < numZeroes + numOnes; i++)
+            input[i] = 1;
+        for (int i = numZeroes + numOnes; i < numZeroes + numOnes + numTwos; i++)
+            input[i] = 2;
+
+    }
+
+    public static void dutchNationalFlagOnePass(int[] a, int mid) {
+        int startIndex = 0;
+        int endIndex = a.length - 1;
+        int temp;
+
+        for (int i = 0; i <= endIndex;) {
+            if (a[i] < mid) {
+                temp = a[i];
+                a[i] = a[startIndex];
+                a[startIndex] = temp;
+                i++;
+                startIndex++;
+            } else if (a[i] > mid) {
+                temp = a[i];
+                a[i] = a[endIndex];
+                a[endIndex] = temp;
+                endIndex--;
+                // do not increment i. We have to revisit this again
+            } else {
+                i++;
+            }
+        }
+    }
+
+    public static int countBinaryOnes(int number) {
+        String binaryString = Integer.toBinaryString(number);
+        return binaryString.length() - binaryString.replace("1", "").length();
+    }
+
     public static String getBinaryRepresentation(int number) {
         int b[] = new int[32];
         int i = 0;
@@ -168,7 +162,7 @@ public class NumberArrays {
         }
         return sb.toString();
     }
-        
+
     public static List<Set<Integer>> powerSet(int[] array) {
         List<Set<Integer>> returnList = new ArrayList<Set<Integer>>();
         if (array.length == 0) {
@@ -190,14 +184,14 @@ public class NumberArrays {
         }
         return returnList;
     }
-    
+
     public static double calcSquareRoot(double num) {
         double guess = 1;
         double diff = Math.abs(guess * guess - num);
         while (diff > 0.0001) {
             diff = Math.abs(guess * guess - num);
             guess = (guess + num / guess) / 2;
-            //System.out.println("New Guess is [" + guess + "]");
+            // System.out.println("New Guess is [" + guess + "]");
         }
         return guess;
     }
