abbd9d08934e60780e91fdd3cdfbe7acc2c6e336
fkelly <frank.kelly@nokia.com>
2013-10-18 15:48:46 -0400

    Reformat text

--- src/main/java/com/kellyfj/codingkata/trees/BinarySearchTreeNode.java
+++ src/main/java/com/kellyfj/codingkata/trees/BinarySearchTreeNode.java
@@ -9,17 +9,16 @@ public class BinarySearchTreeNode {
     protected BinarySearchTreeNode right;
 
     /**
-     * Recurse down the tree until you find a,b
-     * Then return a non-null and start unwinding the stack upwards until both
-     * left and right branches are not null and that node is your answer
+     * Recurse down the tree until you find a,b Then return a non-null and start
+     * unwinding the stack upwards until both left and right branches are not
+     * null and that node is your answer
      * 
      * @param subTreeHead
      * @param a
      * @param b
      * @return
      */
-    public static BinarySearchTreeNode lowestCommonAncestor(BinarySearchTreeNode subTreeHead, 
-            BinarySearchTreeNode a,
+    public static BinarySearchTreeNode lowestCommonAncestor(BinarySearchTreeNode subTreeHead, BinarySearchTreeNode a,
             BinarySearchTreeNode b) {
 
         if (subTreeHead == null)
@@ -37,22 +36,22 @@ public class BinarySearchTreeNode {
 
     }
 
-    private static BinarySearchTreeNode sortedArrayToBST(int arr[], int start, int end) {
-        if (start > end) return null;
-        
-        // same as (start+end)/2, avoids overflow.
-        int mid = start + (end - start) / 2;
-        BinarySearchTreeNode node = new BinarySearchTreeNode(arr[mid]);
-        node.setLeft(sortedArrayToBST(arr, start, mid-1));
-        node.setRight(sortedArrayToBST(arr, mid+1, end));
-        return node;
-      }
-       
     public static BinarySearchTreeNode fromArray(int[] arr) {
         Arrays.sort(arr);
-        return sortedArrayToBST(arr, 0, arr.length-1);
-      }
-      
+        return sortedArrayToBST(arr, 0, arr.length - 1);
+    }
+
+    private static BinarySearchTreeNode sortedArrayToBST(int arr[], int start, int end) {
+        if (start > end)
+            return null;
+
+        int mid = start + (end - start) / 2;
+        BinarySearchTreeNode node = new BinarySearchTreeNode(arr[mid]);
+        node.setLeft(sortedArrayToBST(arr, start, mid - 1));
+        node.setRight(sortedArrayToBST(arr, mid + 1, end));
+        return node;
+    }
+
     public BinarySearchTreeNode(int value) {
         this.value = value;
     }
@@ -128,53 +127,57 @@ public class BinarySearchTreeNode {
                 return node;
             else if (sizeOfRightSubtree < count) {
                 node = node.getLeft();
-                count -= sizeOfRightSubtree + 1;
+                count = count - (sizeOfRightSubtree + 1);
             } else {
                 node = node.getRight();
             }
         }
         return null;
     }
-    
+
     public int getSize() {
         int leftSize = left == null ? 0 : left.getSize();
         int rightSize = right == null ? 0 : right.getSize();
         return 1 + leftSize + rightSize;
     }
-    
+
     @Override
     public boolean equals(Object a) {
-        if(a==null) return false;
-        if(! (a instanceof BinarySearchTreeNode)) return false;
-        BinarySearchTreeNode b = (BinarySearchTreeNode)a;
+        if (a == null)
+            return false;
+        if (!(a instanceof BinarySearchTreeNode))
+            return false;
+        BinarySearchTreeNode b = (BinarySearchTreeNode) a;
         return b.getValue() == this.getValue();
-        
+
     }
-    
+
     @Override
     public String toString() {
         LinkedBlockingQueue<BinarySearchTreeNode> q = new LinkedBlockingQueue<BinarySearchTreeNode>();
         final BinarySearchTreeNode DUMMY = new BinarySearchTreeNode(Integer.MAX_VALUE);
-        
+
         q.add(this);
         q.add(DUMMY);
         StringBuilder sb = new StringBuilder();
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             BinarySearchTreeNode node = q.poll();
-            
+
             if (node.equals(DUMMY)) {
                 if (!q.isEmpty()) {
                     q.add(DUMMY);
                 }
-               sb.append("\n"); 
+                sb.append("\n");
             } else {
-                sb.append(node.getValue()+ " ");
-                if(node.getLeft() != null) q.add(node.getLeft());
-                if(node.getRight() != null) q.add(node.getRight());
+                sb.append(node.getValue() + " ");
+                if (node.getLeft() != null)
+                    q.add(node.getLeft());
+                if (node.getRight() != null)
+                    q.add(node.getRight());
             }
-            
+
         }
-        
+
         return sb.toString();
     }
 }
