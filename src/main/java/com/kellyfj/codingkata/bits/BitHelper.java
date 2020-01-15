2e787bae9d90d7409b9454b0e6414a555e8b1444
Frank Kelly <frank.kelly@here.com>
2020-01-15 11:48:56 -0500

    EPIJ 4.1 parity, 4.3 reverse bits

--- src/main/java/com/kellyfj/codingkata/bits/BitHelper.java
+++ src/main/java/com/kellyfj/codingkata/bits/BitHelper.java
@@ -1,20 +1,116 @@
 package com.kellyfj.codingkata.bits;
 
-
 public class BitHelper {
 
-	public static int chkEnabledBits(int chkInt) {
+    /**
+     * Time Complexity: O(n) where n is number of bits set
+     */
+	public static int countEnabledBits(int number) {
 		int numEnabledBits = 0;
 
-		while (chkInt > 0) {
-			numEnabledBits += chkInt & 1;
-			chkInt = chkInt >> 1;
+		while (number > 0) {
+			numEnabledBits += number & 1;
+			number = number >> 1;
 		}
 
 		return numEnabledBits;
 	}
+	
+    /**
+     * EPIJ 4.1: Compute the Parity of a word
+     */
 
-	public static int chkEnabledBitsTwosComplement(int chkInt) {
+    /**
+     * Time Complexity: O(n)
+     */
+    public static int getParityOrderN(int number) {
+        return countEnabledBits(number) & 1;
+    }
+	
+	/**
+	 * Time Complexity: O(log n)
+	 */
+	public static int getParityOrderLogN(int number) {
+	    boolean parity = false; 
+	    
+        while (number > 0) {
+            parity = !parity; 
+            number = number & (number-1); //Unset rightmost set bit
+        }
+
+        
+        return parity ? 1 : 0;
+	}
+	
+    private static int[] precomputedParity;
+
+    static {
+        precomputedParity = new int[1 << 16];
+        for (int i = 0; i < (1 << 16); ++i) {
+            precomputedParity[i] = getParityOrderLogN(i);
+        }
+    }
+
+    /**
+     * NOTE: We switched to using Long not int (long is 64-bit, int is 32-bit)
+     * @param x
+     * @return
+     */
+    public static int getParityOrder1(long x) {
+        final int WORD_SIZE = 16; //long has four 16-bit words
+        final int BIT_MASK = 0xFFFF; //Used to extract the last bits
+        return (short) 
+                 (precomputedParity[(int) ((x >>> (3 * WORD_SIZE)) & BIT_MASK)]
+                ^ precomputedParity[(int) ((x >>> (2 * WORD_SIZE)) & BIT_MASK)]
+                ^ precomputedParity[(int) ((x >>> WORD_SIZE) & BIT_MASK)] 
+                ^ precomputedParity[(int) (x & BIT_MASK)]);
+    }
+    
+    /**
+     * EPIJ 4.3 Reverse Bits
+     */
+    public static long swapBits(long x, int i, int j) {
+        // Extract the i-th and j-th bits, and see if they differ.
+        long ithBit = x >>> i;
+        long jthBit = x >>> j;
+
+        if ((ithBit & 1) != (jthBit & 1)) {
+            // i-th and j-th bits differ. We will swap them by flipping their values.
+            // Select the bits to flip with bitMask. Since x^1 = 0 when x = 1 and 1
+            // when x = 0, we can perform the flip XOR.
+            long bitMask = (1L << i) | (1L << j);
+            x ^= bitMask;
+        }
+        return x;
+    }
+
+    private static long[] precomputedReverse = new long[(1 << 16)];
+
+    private static long reverseBits(long x, int n) {
+        for (int i = 0, j = n; i < j; ++i, --j) {
+            x = swapBits(x, i, j);
+        }
+        return x;
+    }
+
+    static {
+        for (int i = 0; i < (1 << 16); ++i) {
+            precomputedReverse[i] = reverseBits(i, 15);
+        }
+    }
+
+    public static long reverseBits(long x) {
+      final int WORD_SIZE = 16;
+      final int BIT_MASK = 0xFFFF;
+      return   precomputedReverse[(int)(x & BIT_MASK)] << (3 * WORD_SIZE)
+             | precomputedReverse[(int)((x >>> WORD_SIZE) & BIT_MASK)] << (2 * WORD_SIZE)
+             | precomputedReverse[(int)((x >>> (2 * WORD_SIZE)) & BIT_MASK)] << WORD_SIZE
+             | precomputedReverse[(int)((x >>> (3 * WORD_SIZE)) & BIT_MASK)];
+    }
+    
+    /****************
+     */
+	public static int countEnabledBitsTwosComplement(int chkInt) {
 		int numEnabledBits = 0;
 		int numChkedBits = 0;
 		while ((chkInt != 0) && (numChkedBits < 32)) {
--- src/test/java/com/kellyfj/codingkata/bits/BitHelperTest.java
+++ src/test/java/com/kellyfj/codingkata/bits/BitHelperTest.java
@@ -4,30 +4,100 @@ import junit.framework.TestCase;
 
 public class BitHelperTest extends TestCase {
 
-	public void testBitsChecked() {		
-		int i = BitHelper.chkEnabledBits(7);
-		assertEquals(3,i);
-		
-		i = BitHelper.chkEnabledBits(Integer.MAX_VALUE);
-		assertEquals(31,i);
-	}
+    public void testBitsChecked() {
+        int i = BitHelper.countEnabledBits(7);
+        assertEquals(3, i);
+
+        i = BitHelper.countEnabledBits(Integer.MAX_VALUE);
+        assertEquals(31, i);
+
+        i = BitHelper.countEnabledBits(0);
+        assertEquals(0, i);
+    }
+    
+    public void testParityOrderN() {
+        int i = BitHelper.getParityOrderN(0);
+        assertEquals(0, i);
+        
+        i = BitHelper.getParityOrderN(7);
+        assertEquals(1, i);
+        
+        i = BitHelper.getParityOrderN(Integer.MAX_VALUE - 1);
+        assertEquals(0, i);
+        
+        i = BitHelper.getParityOrderN(Integer.MAX_VALUE);
+        assertEquals(1, i);      
+    }
+    
+    public void testParityOrderLogN() {
+        int i = BitHelper.getParityOrderLogN(0);
+        assertEquals(0, i);
+        
+        i = BitHelper.getParityOrderLogN(7);
+        assertEquals(1, i);
+        
+        i = BitHelper.getParityOrderLogN(Integer.MAX_VALUE - 1);
+        assertEquals(0, i);
+        
+        i = BitHelper.getParityOrderLogN(Integer.MAX_VALUE);
+        assertEquals(1, i);      
+    }
+    
+    
+    public void testParityOrder1() {
+        int i = BitHelper.getParityOrder1(0);
+        assertEquals(0, i);
+        
+        i = BitHelper.getParityOrder1(7);
+        assertEquals(1, i);
+        
+        i = BitHelper.getParityOrder1(Integer.MAX_VALUE - 1);
+        assertEquals(0, i);
+        
+        i = BitHelper.getParityOrder1(Integer.MAX_VALUE);
+        assertEquals(1, i);     
+        
+        i = BitHelper.getParityOrder1(Long.MAX_VALUE - 1);
+        assertEquals(0, i);
+        
+        i = BitHelper.getParityOrder1(Long.MAX_VALUE);
+        assertEquals(1, i); 
+        
+    } 
 	
+    public void testSwapBits() {
+        assert (BitHelper.swapBits(47, 1, 4) == 61);
+        assert (BitHelper.swapBits(28, 0, 2) == 25);
+    }
+    
+    public void testReverseBits() {
+        long l = BitHelper.reverseBits(0);
+        assertEquals(0, l);
+
+        l = BitHelper.reverseBits(1);
+        assertEquals("1000000000000000000000000000000000000000000000000000000000000000", Long.toBinaryString(l));
+
+        l = BitHelper.reverseBits(7);
+        assertEquals("1110000000000000000000000000000000000000000000000000000000000000", Long.toBinaryString(l));
+
+    }
+    
 	public void testBitsCheckedTwosComp() {	
 		
 		//0b111
-		int i = BitHelper.chkEnabledBitsTwosComplement(7);
+		int i = BitHelper.countEnabledBitsTwosComplement(7);
 		assertEquals(3,i);
 		
 		//0b011.....1 = 31 bits set
-		i = BitHelper.chkEnabledBitsTwosComplement(Integer.MAX_VALUE);
+		i = BitHelper.countEnabledBitsTwosComplement(Integer.MAX_VALUE);
 		assertEquals(31,i);
 		
 		//0b100000.....000 = MSB set
-		i = BitHelper.chkEnabledBitsTwosComplement(Integer.MIN_VALUE);
+		i = BitHelper.countEnabledBitsTwosComplement(Integer.MIN_VALUE);
 		assertEquals(1,i);
 		
 		//All bits set
-		i = BitHelper.chkEnabledBitsTwosComplement(-1);
+		i = BitHelper.countEnabledBitsTwosComplement(-1);
 		assertEquals(32,i);
 	}
 	
